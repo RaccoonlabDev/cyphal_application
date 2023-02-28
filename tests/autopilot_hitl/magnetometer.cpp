a0f7cba06fea768e41ce135275bf2e7fb4a5545d
Dmitry Ponomarev <ponomarevda96@gmail.com>
2023-02-28 23:57:19 +0300

    add magnetometer

---
+++ tests/autopilot_hitl/magnetometer.cpp
@@ -0,0 +1,14 @@
+/// This software is distributed under the terms of the MIT License.
+/// Copyright (c) 2023 Dmitry Ponomarev.
+/// Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+
+#include "magnetometer.hpp"
+
+void MagneticFieldPublisher::publish(const uavcan_si_sample_magnetic_field_strength_Vector3_1_0& msg) {
+    static uint8_t buffer[uavcan_si_sample_magnetic_field_strength_Vector3_1_0_EXTENT_BYTES_];
+    size_t buffer_size = uavcan_si_sample_magnetic_field_strength_Vector3_1_0_EXTENT_BYTES_;
+    int32_t result = uavcan_si_sample_magnetic_field_strength_Vector3_1_0_serialize_(&msg, buffer, &buffer_size);
+    if (NUNAVUT_SUCCESS == result) {
+        push(buffer_size, buffer);
+    }
+}
---
+++ tests/autopilot_hitl/magnetometer.hpp
@@ -0,0 +1,16 @@
+/// This software is distributed under the terms of the MIT License.
+/// Copyright (c) 2023 Dmitry Ponomarev.
+/// Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+
+#ifndef MAGNETOMETER_HPP_
+#define MAGNETOMETER_HPP_
+
+#include "cyphal.hpp"
+#include "uavcan/si/sample/magnetic_field_strength/Vector3_1_0.h"
+
+struct MagneticFieldPublisher: public CyphalPublisher {
+    MagneticFieldPublisher(Cyphal* driver_, CanardPortID port_id) : CyphalPublisher(driver_, port_id) {};
+    void publish(const uavcan_si_sample_magnetic_field_strength_Vector3_1_0& msg);
+};
+
+#endif  // MAGNETOMETER_HPP_
