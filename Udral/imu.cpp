59bbee2dfd3e962b7c638898850d20985f4692ad
PonomarevDA <ponomarevda96@gmail.com>
2023-10-02 21:57:59 +0300

    optimize imu (use unit instead of sample)

--- Udral/imu.cpp
+++ Udral/imu.cpp
@@ -6,23 +6,23 @@
 #include "main.h"
 #include "params.hpp"
 
-void ImuAccelPublisher::publish(const uavcan_si_sample_acceleration_Vector3_1_0& msg) {
+void ImuAccelPublisher::publish(const uavcan_si_unit_acceleration_Vector3_1_0& msg) {
     setPortId(paramsGetIntegerValue(IntParamsIndexes::IMU_ACCEL_ID));
 
-    static uint8_t buffer[uavcan_si_sample_acceleration_Vector3_1_0_EXTENT_BYTES_];
-    size_t buffer_size = uavcan_si_sample_acceleration_Vector3_1_0_EXTENT_BYTES_;
-    int32_t result = uavcan_si_sample_acceleration_Vector3_1_0_serialize_(&msg, buffer, &buffer_size);
+    static uint8_t buffer[uavcan_si_unit_acceleration_Vector3_1_0_EXTENT_BYTES_];
+    size_t buffer_size = uavcan_si_unit_acceleration_Vector3_1_0_EXTENT_BYTES_;
+    int32_t result = uavcan_si_unit_acceleration_Vector3_1_0_serialize_(&msg, buffer, &buffer_size);
     if (NUNAVUT_SUCCESS == result) {
         push(buffer_size, buffer);
     }
 }
 
-void ImuGyroPublisher::publish(const uavcan_si_sample_angular_velocity_Vector3_1_0& msg) {
+void ImuGyroPublisher::publish(const uavcan_si_unit_angular_velocity_Vector3_1_0& msg) {
     setPortId(paramsGetIntegerValue(IntParamsIndexes::IMU_GYRO_ID));
 
-    static uint8_t buffer[uavcan_si_sample_angular_velocity_Vector3_1_0_EXTENT_BYTES_];
-    size_t buffer_size = uavcan_si_sample_angular_velocity_Vector3_1_0_EXTENT_BYTES_;
-    int32_t result = uavcan_si_sample_angular_velocity_Vector3_1_0_serialize_(&msg, buffer, &buffer_size);
+    static uint8_t buffer[uavcan_si_unit_angular_velocity_Vector3_1_0_EXTENT_BYTES_];
+    size_t buffer_size = uavcan_si_unit_angular_velocity_Vector3_1_0_EXTENT_BYTES_;
+    int32_t result = uavcan_si_unit_angular_velocity_Vector3_1_0_serialize_(&msg, buffer, &buffer_size);
     if (NUNAVUT_SUCCESS == result) {
         push(buffer_size, buffer);
     }
--- Udral/imu.hpp
+++ Udral/imu.hpp
@@ -6,17 +6,17 @@
 #define UDRAL_IMU_HPP_
 
 #include "cyphal.hpp"
-#include "uavcan/si/sample/angular_velocity/Vector3_1_0.h"
-#include "uavcan/si/sample/acceleration/Vector3_1_0.h"
+#include "uavcan/si/unit/angular_velocity/Vector3_1_0.h"
+#include "uavcan/si/unit/acceleration/Vector3_1_0.h"
 
 struct ImuAccelPublisher: public CyphalPublisher {
     ImuAccelPublisher(Cyphal* driver_, CanardPortID port_id) : CyphalPublisher(driver_, port_id) {};
-    void publish(const uavcan_si_sample_acceleration_Vector3_1_0& msg);
+    void publish(const uavcan_si_unit_acceleration_Vector3_1_0& msg);
 };
 
 struct ImuGyroPublisher: public CyphalPublisher {
     ImuGyroPublisher(Cyphal* driver_, CanardPortID port_id) : CyphalPublisher(driver_, port_id) {};
-    void publish(const uavcan_si_sample_angular_velocity_Vector3_1_0& msg);
+    void publish(const uavcan_si_unit_angular_velocity_Vector3_1_0& msg);
 };
 
 #endif  // UDRAL_IMU_HPP_
