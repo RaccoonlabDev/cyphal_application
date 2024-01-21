5ad29a33291b020bed41374f25b0676e5a727c2a
PonomarevDA <ponomarevda96@gmail.com>
2024-01-21 14:53:51 +0300

    fix script

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This software is distributed under the terms of the MIT License.
+# Copyright (c) 2023 Dmitry Ponomarev.
+# Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
 
 if [ "${BASH_SOURCE[0]}" -ef "$0" ]
 then
--- scripts/nnvg_generate_c_headers.sh
+++ scripts/nnvg_generate_c_headers.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
+# This software is distributed under the terms of the MIT License.
+# Copyright (c) 2023-2024 Dmitry Ponomarev.
+# Author: Dmitry Ponomarev <ponomarevda96@gmail.com>
+SCRIPT_NAME=$(basename $BASH_SOURCE)
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PUBLIC_DSDL_DIR="$SCRIPT_DIR/../Libs/public_regulated_data_types"
-DS015_DSDL_DIR="$SCRIPT_DIR/../Libs/ds015"
+REPOSITORY_PATH="$(dirname "$SCRIPT_DIR")"
+PUBLIC_DSDL_DIR="$REPOSITORY_PATH/Libs/public_regulated_data_types"
+DS015_DSDL_DIR="$REPOSITORY_PATH/Libs/ds015"
 
-if [ ! -d $1 ]; then
-    OUT_DIR=$1 # custom build directory
+if [ "$#" -eq 1 ]; then
+    OUT_DIR=$1
 else
-    OUT_DIR="$SCRIPT_DIR/../build/nunavut_out"
+    OUT_DIR="$REPOSITORY_PATH/build/nunavut_out"
 fi
 
 mkdir -p $OUT_DIR
